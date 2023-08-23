f65e4dd33db5db57d71a9bf1710c5f6ea6ae16b1
정효은 <junghyoeun@jeonghyoeun-ui-MacBookPro.local>
2023-08-23 16:18:01 +0900

    Add : 로그인 기능구현

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './Login.scss';
-import { Link } from 'react-router-dom';
 
 export default function Login() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const saveEmail = e => {
+    setEmail(e.target.value);
+  };
+
+  const savePassword = e => {
+    setPassword(e.target.value);
+  };
+
+  const goToHome = () => {
+    navigate('/');
+    fetch('http://10.58.52.144:3000/users/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.accessToken) {
+          localStorage.setItem('token', data.accessToken);
+          navigate('/');
+        }
+        console.log(data.message);
+        console.log(data.accessToken);
+        if (data.message === 'specified user does not exist') {
+          alert('존재하지 않는 유저입니다.');
+        }
+      });
+  };
+
+  const isInputValue = email.includes('@') && password.length >= 8;
+
   return (
-    <div className="containerWrap">
+    <div className="login">
       <main className="container">
         <div className="logoWrap">
           <div className="logo">
@@ -14,15 +54,29 @@ export default function Login() {
             </div>
 
             <form className="form">
-              <input className="email" type="email" placeholder="이메일" />
+              <input
+                className="email"
+                type="email"
+                placeholder="이메일"
+                onChange={saveEmail}
+                value={email}
+              />
 
               <input
                 className="password"
                 type="password"
                 placeholder="비밀번호"
+                onChange={savePassword}
+                value={password}
               />
 
-              <button className="btn">로그인</button>
+              <button
+                className={isInputValue ? 'btn active' : 'btn'}
+                onClick={goToHome}
+                disabled={!isInputValue}
+              >
+                로그인
+              </button>
             </form>
             <div className="rePassword">
               <a>비밀번호 재설정 </a>

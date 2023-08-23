2ed79a1f84b7679dcd8910ae3a7349d529fd1568
정효은 <junghyoeun@jeonghyoeun-ui-MacBookPro.local>
2023-08-23 14:50:06 +0900

    Fix: reset, common.scss 적용되어있지 않던 문제 수정

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './styles/reset.scss';
+import './styles/common.scss';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

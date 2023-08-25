b537ac3031ce141baa11eea42d3cda2ba929cc06
정효은 <junghyoeun@jeonghyoeun-ui-MacBookPro.local>
2023-08-25 15:17:23 +0900

    Add: banner 컴포넌트 수정, 카테고리 컴포넌트 구현 중

--- src/pages/Home/components/Banner.jsx
+++ src/pages/Home/components/Banner.jsx
@@ -2,67 +2,50 @@ import React, { useState, useEffect } from 'react';
 import './Banner.scss';
 
 const Banner = () => {
-  const images = [
-    {
-      id: '라이언',
-      url: 'https://cdn.pixabay.com/photo/2015/10/20/18/57/furniture-998265_1280.jpg',
-      text: '10평대 전세집 최소수정',
-    },
-    {
-      id: '토마토',
-      url: 'https://cdn.pixabay.com/photo/2017/03/28/12/17/chairs-2181994_1280.jpg',
-      text: '20평대 전세집',
-    },
-    {
-      id: '춘식이',
-      url: 'https://cdn.pixabay.com/photo/2017/07/09/03/19/home-2486092_1280.jpg',
-      text: '30평대 전세집',
-    },
-    {
-      id: '햄스터',
-      url: 'https://cdn.pixabay.com/photo/2017/08/27/10/16/interior-2685522_1280.jpg',
-      text: '40평대 전세집',
-    },
-    {
-      id: '애플',
-      url: 'https://cdn.pixabay.com/photo/2014/08/11/21/39/wall-416060_1280.jpg',
-      text: '50평대 전세집',
-    },
-    {
-      id: '디지몬',
-      url: 'https://cdn.pixabay.com/photo/2018/07/15/13/04/living-room-3539587_1280.jpg',
-      text: '무료배솔',
-    },
-  ];
-
   const [count, setCount] = useState(0);
+  const [bannerImages, setBannerImages] = useState([]);
+
   const handlePrev = () => {
-    setCount(prevCount => (prevCount - 1 + images.length) % images.length);
+    setCount(
+      prevCount =>
+        (prevCount - 1 + BANNER_IMAGES.length) % BANNER_IMAGES.length,
+    );
   };
   const handleNext = () => {
-    setCount(prevCount => (prevCount + 1) % images.length);
+    setCount(prevCount => (prevCount + 1) % BANNER_IMAGES.length);
   };
 
   useEffect(() => {
-    const timer = setTimeout(handleNext, 1000);
+    fetch('/data/bannerData.json')
+      .then(res => res.json())
+      .then(result => setBannerImages(result));
+  }, []);
+
+  useEffect(() => {
+    const timer = setTimeout(handleNext, 5000);
+
     return () => clearTimeout(timer);
   }, [count]);
+
+  // component early return
+  // if (bannerImages.length === 0) return null;
 
   return (
     <div className="banner">
       <div className="imgContainer">
-        <img className="images" src={images[count].url} alt="Banner" />
+        {/* optional chaining */}
+        <img className="images" src={bannerImages[count]?.url} alt="Banner" />
+        <button className="btn prev" onClick={handlePrev}>
+          &lt;
+        </button>
+        <button className="btn next" onClick={handleNext}>
+          &gt;
+        </button>
         <div className="bannerTextWrap">
-          <p className="bannerTitle">{images[count].text}</p>
-          <p className="bannerId">{images[count].id}</p>
+          <p className="bannerTitle">{bannerImages[count]?.text}</p>
+          <p className="bannerId">{bannerImages[count]?.id}</p>
         </div>
       </div>
-      <button className="btn" onClick={handlePrev}>
-        &lt;
-      </button>
-      <button className="btn" onClick={handleNext}>
-        &gt;
-      </button>
     </div>
   );
 };
---
+++ src/pages/Home/components/Categories.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Categories.scss';
+
+const Categories = () => {
+  return <div>Categories</div>;
+};
+
+export default Categories;
+
+const categori = [
+  {
+    name: '똑똑하게 잘꾸민 거실',
+    image:
+      'https://cdn.pixabay.com/photo/2013/09/21/14/30/sofa-184551_1280.jpg',
+    title: '편안한게 최고',
+    text: '쇼파 짱',
+  },
+];

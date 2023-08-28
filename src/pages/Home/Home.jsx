9cccf4dfd3387082626fa3022549bbe83d42251a
정효은 <junghyoeun@jeonghyoeun-ui-MacBookPro.local>
2023-08-28 09:58:53 +0900

    Modify : categories 컴포넌트 작업중

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,6 +8,8 @@ export default function Home() {
     <div className="home">
       <Banner />
       <Categories />
+      <Categories />
+      <Categories />
     </div>
   );
 }
--- src/pages/Home/components/Categories.jsx
+++ src/pages/Home/components/Categories.jsx
@@ -1,28 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Categories.scss';
 
 const Categories = () => {
+  const IMAGES_PER_VIEW = 4;
+  const [pictures, setPictures] = useState(0);
+
+  const handleNext = () => {
+    if (pictures + IMAGES_PER_VIEW < MOCK_DATA.length) {
+      setPictures(pictures + IMAGES_PER_VIEW);
+    } else {
+      setPictures(0);
+    }
+  };
+
+  const handlePrev = () => {
+    if (pictures - IMAGES_PER_VIEW >= 0) {
+      setPictures(pictures - IMAGES_PER_VIEW);
+    } else {
+      const remainder = MOCK_DATA.length % IMAGES_PER_VIEW;
+      setPictures(
+        remainder === 0
+          ? MOCK_DATA.length - IMAGES_PER_VIEW
+          : MOCK_DATA.length - remainder,
+      );
+    }
+  };
+
+  let displayedImages = MOCK_DATA.slice(pictures, pictures + IMAGES_PER_VIEW);
+
   return (
-    <div>
-      <p className="hash">#{MOCK_DATA[0].hash}</p>
-      <button className="btn prev">&lt;</button>
-      <button className="btn next">&gt;</button>
+    <div className="categoriesWrapper">
+      <p className="hash">#{displayedImages[0]?.hash}</p>
+      <button className="btn prev" onClick={handlePrev}>
+        &lt;
+      </button>
       <div className="categoriesContainer">
-        {MOCK_DATA.map(item => (
+        {displayedImages.map(item => (
           <div className="categoryCard" key={item.image}>
             <img className="images" src={item.image} alt={item.title} />
-
             <h2 className="title">{item.title}</h2>
             <p className="text">{item.text}</p>
           </div>
         ))}
       </div>
+      <button className="btn next" onClick={handleNext}>
+        &gt;
+      </button>
     </div>
   );
 };
 
 export default Categories;
-
 const MOCK_DATA = [
   {
     hash: '똑똑하게 잘꾸민 거실',
@@ -56,5 +84,37 @@ const MOCK_DATA = [
     title: '사무실~',
     text: 'ㅇㅇㄴㅁㄹㅇㅁㄹ',
     user: '맥북~',
+  },
+  {
+    hash: '똑똑하게 잘꾸민 거실',
+    image:
+      'https://cdn.pixabay.com/photo/2019/03/11/09/58/ikea-4048225_1280.jpg',
+    title: '공부 방~~',
+    text: '이케아꺼래~',
+    user: '키보드~',
+  },
+  {
+    hash: '똑똑하게 잘꾸민 거실',
+    image:
+      'https://cdn.pixabay.com/photo/2019/03/11/09/58/ikea-4048225_1280.jpg',
+    title: '공부 방~~',
+    text: '이케아꺼래~',
+    user: '키보드~',
+  },
+  {
+    hash: '똑똑하게 잘꾸민 거실',
+    image:
+      'https://cdn.pixabay.com/photo/2019/03/11/09/58/ikea-4048225_1280.jpg',
+    title: '공부 방~~',
+    text: '이케아꺼래~',
+    user: '키보드~',
+  },
+  {
+    hash: '똑똑하게 잘꾸민 거실',
+    image:
+      'https://cdn.pixabay.com/photo/2019/03/11/09/58/ikea-4048225_1280.jpg',
+    title: '공부 방~~',
+    text: '이케아꺼래~',
+    user: '키보드~',
   },
 ];

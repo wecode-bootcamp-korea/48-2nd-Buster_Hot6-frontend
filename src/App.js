41de494dcff8d009c0f53bdb411db48fb9494de0
정효은 <junghyoeun@jeonghyoeun-ui-MacBookPro.local>
2023-08-28 16:47:30 +0900

    add : 레이아웃 구현

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <Home /> },
       { path: '/store', element: <Store /> },
+      { path: '/detail/:id', element: <Store /> },
     ],
   },
 ]);
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.scss';
 import Banner from './components/Banner';
 import Categories from './components/Categories';
 
 export default function Home() {
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    fetch('/data/categories.json')
+      .then(res => res.json())
+      .then(result => setCategories(result.data));
+  }, []);
+
+  if (!categories.length > 0) return null;
   return (
     <div className="home">
       <Banner />
-      <Categories />
-      <Categories />
-      <Categories />
+      {categories.map(category => {
+        return <Categories key={category.id} category={category} />;
+      })}
     </div>
   );
 }

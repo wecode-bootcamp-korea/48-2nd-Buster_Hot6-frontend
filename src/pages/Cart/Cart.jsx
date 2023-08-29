ee859ae02dfd6d48fe1c811b5a0904b2944dc22d
정효은 <junghyoeun@jeonghyoeun-ui-MacBookPro.local>
2023-08-29 17:55:37 +0900

    Modify : cart ui 수정

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,19 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Cart.scss';
 
 const Cart = () => {
-  // const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
 
-  // useEffect(() => {
-  //   fetch('/data/cart.json')
-  //     .then(response => response.json())
-  //     .then(data => setCartItems(data))
-  //     .catch(error => console.error('Fetching data error:', error));
-  // }, []);
-
-  // const handleCheckout = () => {
-  //   alert('결제가 진행됩니다.');
-  // };
+  useEffect(() => {
+    fetch('/data/cart.json')
+      .then(response => response.json())
+      .then(data => setCartItems(data));
+  }, []);
 
   return (
     <div className="cart">
@@ -21,8 +16,8 @@ const Cart = () => {
       <div className="cardList">
         <div className="today">오늘 출발</div>
         <div className="product">
-          <img className="img" src="#" alt="상품 이미지" />{' '}
-          {/* 이미지 src를 지정하십시오 */}
+          <img className="img" src="#" alt="상품 이미지" />
+
           <span className="productName">[코튼리빙]40수 호텔수건</span>
         </div>
 
@@ -35,11 +30,11 @@ const Cart = () => {
       <div className="payModal">
         <div className="modal">
           <div>총 상품 금액</div>
-          <div>얼마</div>
+          <div>29,000</div>
         </div>
         <div className="modal">
           <div>총 배송비</div>
-          <div>얼마</div>
+          <div>무료</div>
         </div>
         <div className="modal">
           <div>총 할인금액</div>
@@ -50,7 +45,7 @@ const Cart = () => {
           <div>얼마</div>
         </div>
       </div>
-      <button>결제하기</button>
+      <butto className="paymentButton">결제하기</butto>
     </div>
   );
 };

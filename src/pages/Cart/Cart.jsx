5f9aee2262f7eaa1dbe2c0134eda62a51b7cc4cf
정효은 <junghyoeun@jeonghyoeun-ui-MacBookPro.local>
2023-08-29 17:33:08 +0900

    Add : cart ui 구현

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -2,24 +2,55 @@ import React from 'react';
 import './Cart.scss';
 
 const Cart = () => {
+  // const [cartItems, setCartItems] = useState([]);
+
+  // useEffect(() => {
+  //   fetch('/data/cart.json')
+  //     .then(response => response.json())
+  //     .then(data => setCartItems(data))
+  //     .catch(error => console.error('Fetching data error:', error));
+  // }, []);
+
+  // const handleCheckout = () => {
+  //   alert('결제가 진행됩니다.');
+  // };
+
   return (
-    <div>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>상품명</th>
-          <th>수량</th>
-          <th>변경하기</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>안녕</td>
-          <td>안녕</td>
-          <td>안녕</td>
-        </tr>
-      </tbody>
+    <div className="cart">
+      <h1>브랜드</h1>
+      <div className="cardList">
+        <div className="today">오늘 출발</div>
+        <div className="product">
+          <img className="img" src="#" alt="상품 이미지" />{' '}
+          {/* 이미지 src를 지정하십시오 */}
+          <span className="productName">[코튼리빙]40수 호텔수건</span>
+        </div>
+
+        <div className="modal">
+          <span>40tn 200g 화이트</span>
+          <span className="price">29,000</span>
+        </div>
+        <span className="price">29,000</span>
+      </div>
+      <div className="payModal">
+        <div className="modal">
+          <div>총 상품 금액</div>
+          <div>얼마</div>
+        </div>
+        <div className="modal">
+          <div>총 배송비</div>
+          <div>얼마</div>
+        </div>
+        <div className="modal">
+          <div>총 할인금액</div>
+          <div>얼마</div>
+        </div>
+        <div className="modal">
+          <div>결제금액</div>
+          <div>얼마</div>
+        </div>
+      </div>
+      <button>결제하기</button>
     </div>
   );
 };
